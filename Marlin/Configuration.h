6a9201cbc53fa9bca329ef6666aa83c9d9116e66
Jonas Friedmann <j@frd.mn>
2023-04-16 18:46:10 +0200

    Add build date to status (ready) screen

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -62,7 +62,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(frdmn, Ender-3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "frdmn" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -77,7 +77,7 @@
  */
 
 // Show the Marlin bootscreen on startup. ** ENABLE FOR PRODUCTION **
-// #define SHOW_BOOTSCREEN
+#define SHOW_BOOTSCREEN
 
 // Show the bitmap in Marlin/_Bootscreen.h on startup.
 // #define SHOW_CUSTOM_BOOTSCREEN
@@ -137,7 +137,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3"
+#define CUSTOM_MACHINE_NAME "Marlin " DETAILED_BUILD_VERSION " ("  __DATE__ " " __TIME__ ")"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
